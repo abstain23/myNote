a35eeb258b06aeb0af8102195ce8f3877f4c3620
YangJ0605 <1442122744@qq.com>
2020-06-03 17:43:03 +0800

    learn bind

--- 手写一个bind.js
+++ 手写一个bind.js
@@ -11,3 +11,33 @@ Function.prototype.myBind = function(context) {
 }
 //如果有构造函数
 
+function Person() {
+  this.name = 'p'
+  console.log(this)
+}
+// var a = Person.bind({va:1})
+var p = Person.call({ccc:'call'})
+
+Function.prototype.myBind2 = function(ctx, ...args) {
+  const _this = this
+  const Temp = function() {}
+  const fn = function(...args2) {
+    return _this.apply(this instanceof fn ? this: ctx, [...args, ...args2])
+  }
+  // fn.prototype = _this.prototype  这样当我们直接修改fn.prototype的时候也会修改绑定函数的prototype
+
+  Temp.prototype = _this.prototype
+  fn.prototype = new Temp()
+  return fn
+}
+
+function Person() {
+  this.name = 'p'
+  console.log(this)
+}
+
+Person.prototype.pp ='pp'
+var a = Person.myBind2({ccc:1})
+console.log(new a())
+
+
