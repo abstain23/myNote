55d948eb61e86337ee085bd226b9d131fb2b1e36
YangJ0605 <1442122744@qq.com>
2020-06-03 17:57:39 +0800

    完善new

--- 手写实现new.js
+++ 手写实现new.js
@@ -3,5 +3,5 @@ function copyNew(){
   const Constructor = [...arguments][0]
   obj.__proto__ = Constructor.prototype
   const res = Constructor.apply(obj,[...arguments].slice(1))
-  return typeof res === 'object' ? res : obj
+  return res && (typeof res === 'object') ? res : obj
 }
