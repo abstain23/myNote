d09bffaf7fd88613c80a73b567b6ba42de8d2b65
YangJ0605 <1442122744@qq.com>
2020-06-04 15:35:45 +0800

    html drag

--- promise2.js
+++ promise2.js
@@ -44,3 +44,27 @@ p1.then((value) => {
 // setTimeout(() => {
 //   console.log(p1)
 // },1100)
+
+
+async function async1() {
+  console.log('async1 start');  
+  await async2();
+  console.log('async1 end');
+}
+async function async2() {
+  console.log('async2');
+}
+console.log('script start');
+setTimeout(function() {
+  console.log('setTimeout');
+}, 0)
+async1();
+new Promise(function(resolve) {
+  console.log('promise1');
+  resolve();
+}).then(function() {
+  console.log('promise2');
+});
+console.log('script end');
+
+// async1 start script start promise1  script end  async2 async1 end 
