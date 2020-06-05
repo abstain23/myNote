ef5fa722e2102a3899c9d12fcdb9018af8e9375b
YangJ0605 <1442122744@qq.com>
2020-06-05 23:43:38 +0800

    write promise

--- 实现一个简单的promise.js
+++ 实现一个简单的promise.js
@@ -43,91 +43,108 @@
 /**
  * excutor 执行器函数（同步）
  */
-!function(){
+!function () {
   function Promise(excutor) {
     this.status = 'pendding'
     this.data = undefined
     this.callbacks = [] //{onResloved:() => {}, onRejected:() => {}}
     // let _this = this
-    const resolve = (value)  => {
-      if(this.status !== 'pendding') return
+    const resolve = (value) => {
+      if (this.status !== 'pendding') return
       this.status = 'resolved'
       this.data = value
-      if(this.callbacks.length > 0) {
+      if (this.callbacks.length > 0) {
         setTimeout(() => {
           this.callbacks.forEach(cbObj => {
             cbObj['onResolved'](value)
           })
-        },0)
+        }, 0)
       }
     }
 
     const reject = (reason) => {
-      if(this.status !== 'pendding') return
+      if (this.status !== 'pendding') return
       this.status = 'rejected'
       this.data = reason
-      if(this.callbacks.length > 0) {
+      if (this.callbacks.length > 0) {
         setTimeout(() => {
           this.callbacks.forEach(cbObj => {
             cbObj['onRejected'](value)
           })
-        },0)
+        }, 0)
       }
     }
     try {
       excutor(resolve, reject)
-    }catch(err) {
+    } catch (err) {
       reject(err)
     }
   }
+  
+  Promise.prototype.then = function (onResolved, onRejected) {
 
-  Promise.prototype.then = function(onResolved, onRejected) {
+    onResolved = typeof onResolved === 'function' ? onResolved : value => value
+    onRejected = typeof onRejected === 'function' ? onRejected : reason => {throw reason}
+
     return new Promise((resolve, reject) => {
-      // console.log(this.status)
-      if(this.status === 'pendding') {
-        this.callbacks.push({onResolved, onRejected})
-      } else if(this.status === 'resolved') {
-        setTimeout(() => {
-          try {
-            const res = onResolved(this.data)
-            if(res instanceof Promise) {
-              // res.then(value => resolve(value), err => reject(err))
-              res.then(resolve, reject)
-            } else {
-              resolve(res)
-            }
-          } catch (error) {
-            reject(error)
+      const handle = (callback) => {
+        try {
+          const res = callback(this.data)
+          if (res instanceof Promise) {
+            // res.then(value => resolve(value), err => reject(err))
+            res.then(resolve, reject)
+          } else {
+            resolve(res)
           }
+        } catch (error) {
+          reject(error)
+        }
+      }
+      // console.log(this.status)
+      if (this.status === 'pendding') {
+        this.callbacks.push({
+          onResolved: (value) => {
+            // onResolved(this.data)
+            // onResolved(value)
+            handle(onResolved)
+          },
+          onRejected: (reason) => {
+            // onRejected(reason)
+            handle(onRejected)
+          }
+        })
+      } else if (this.status === 'resolved') {
+        setTimeout(() => {
+          handle(onResolved)
+          // resolve(this.data)
         })
       } else {
         setTimeout(() => {
-          onRejected(this.data)
-        })
+          handle(onRejected)
+        });
       }
     })
   }
 
+    Promise.prototype.catch = function (onRejected) {
+      return this.then(undefined, onRejected)
+    }
 
-  Promise.prototype.catch = function(onRejected) {
+    Promise.resolve = function (value) {
+      
+    }
 
-  }
+    Promise.rejected = function (reason) {
 
-  Promise.resolve = function(value) {
+    }
 
-  }
+    Promise.all = function (promises) {
 
-  Promise.rejected = function(reason) {
+    }
 
-  }
+    Promise.race = function (promises) {
 
-  Promise.all = function(promises) {
-
-  }
-
-  Promise.race = function(promises) {
-
-  }
-  window.Promise = Promise
-}(window)
+    }
+    window.Promise = Promise
+  }(window)
 
