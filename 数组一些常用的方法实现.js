000a70a4a16a9726c86f6c595e41c817798788a6
YangJ0605 <1442122744@qq.com>
2020-06-12 16:58:19 +0800

    实现一些数组的方法

--- 数组一些常用的方法实现.js
+++ 数组一些常用的方法实现.js
@@ -1,4 +1,14 @@
-Array.prototype.push = function() {
+Array.prototype.forEach = function (cb, context = undefined) {
+  let arr = this.slice()
+  let len = arr.length
+  for (let i = 0; i < len; i++) {
+    if (i in arr) {
+      cb.call(context, arr[i], i, arr)
+    }
+  }
+}
+
+Array.prototype.push = function () {
   for(let i = 0; i< arguments.length; i++) {
     this[this.length] = arguments[i]
   }
@@ -25,4 +35,54 @@ Array.prototype.shift = function() {
   return value
 }
 
-Array.prototype.unshift = 
+Array.prototype.map = function (cb, context = undefined) {
+  let arr = this.slice()
+  let len = arr.length
+  let res = []
+  for (let i = 0; i < len; i++) {
+    res[i] = cb.call(context, arr[i], i, arr)
+  }
+  return res
+}
+
+
+Array.prototype.myReduce = function (cb, initData) {
+  let arr = this, len = arr.length
+  let res = initData || arr[0]
+  let startIndex = initData ? 0 : 1
+  for (let i = startIndex; i < len; i++) {
+    res = cb(res, arr[i], i, arr)
+  }
+  return res
+}
+
+Array.prototype.mapByReduce = function (cb, context = null) {
+  return arr.reduce((pre, curr, index, arr) => {
+    let res = cb.call(context, curr, index, arr)
+    return [...pre, res]
+  },[])
+}
+
+Array.prototype.filter = function (cb, context=undefined) {
+  let arr = this.slice()
+  let len = arr.length
+  let res = [], newIndex=0
+  for (let i = 0; i < len; i++) {
+    let flag = cb.call(context, arr[i], i, arr)
+    if (flag) {
+      console.log(i, flag)
+      res[newIndex++] = arr[i] 
+    }
+  }
+  return res
+}
+
+
+Array.prototype.find = function (cb, context) {
+  let arr = this.slice()
+  let len = arr.length
+  for (let i = 0; i < len; i++) {
+    let res = cb.call(context, arr[i], i, arr)
+    if(res) return arr[i]
+  }
+}
